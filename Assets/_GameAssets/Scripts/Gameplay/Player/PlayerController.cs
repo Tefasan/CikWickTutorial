446e3288b1f334fa7101101ef1be538cdaaa26c8
Tefasan <143481775+Tefasan@users.noreply.github.com>
2025-03-06 06:39:46 +0300

    v0.6 Character Animations Added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
@@ -1,8 +1,10 @@
+using System;
 using System.IO.Pipes;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public event Action OnPlayerJumped;
 [Header("References")]
 [SerializeField] private Transform _orientationTransform;
 
@@ -139,6 +141,7 @@ public class PlayerController : MonoBehaviour
     }
     private void SetPlayerJumping()
     {
+        OnPlayerJumped?.Invoke();
         _playerRigidbody.linearVelocity = new Vector3 (_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
         _playerRigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
     }
