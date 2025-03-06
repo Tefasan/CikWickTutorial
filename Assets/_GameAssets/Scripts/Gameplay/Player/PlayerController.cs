75c581346a7b137e1e59b29e68dec480aa3a5b8e
Tefasan <143481775+Tefasan@users.noreply.github.com>
2025-03-06 20:59:40 +0300

    v.0.10 Boostable Spatula Added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
@@ -194,5 +194,11 @@ public class PlayerController : MonoBehaviour
     {
         _jumpForce = _startingJumpForce;
     }
+
+    public Rigidbody GetPlayerRigidbody()
+    {
+        return _playerRigidbody;
+    }
+    
     #endregion
 }
