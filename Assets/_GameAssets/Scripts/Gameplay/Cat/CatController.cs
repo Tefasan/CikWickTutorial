4e193bb8fd244cceac8b8cb7bf06ebb7b5db0661
Tefasan <143481775+Tefasan@users.noreply.github.com>
2025-03-09 21:44:08 +0300

    v0.20 Cat Animations & Lose Situations Added

--- Assets/_GameAssets/Scripts/Gameplay/Cat/CatController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Cat/CatController.cs
@@ -1,9 +1,11 @@
+using System;
 using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class CatController : MonoBehaviour
 {
+    public event Action OnCatCatched;
     [Header("References")]
     [SerializeField] private PlayerController _playerController; 
     [SerializeField] private Transform _playerTransform;
@@ -55,6 +57,7 @@ public class CatController : MonoBehaviour
     }
     private void SetChaseMovement()
     {
+        _isChasing = true;
         Vector3 directionToPlayer = (_playerTransform.position - transform.position).normalized;
         Vector3 offsetPosition = _playerTransform.position - directionToPlayer * _chaseDistanceThreshold;
         _catAgent.SetDestination(offsetPosition);
@@ -64,6 +67,7 @@ public class CatController : MonoBehaviour
         if(Vector3.Distance(transform.position, _playerTransform.position) <= _chaseDistance && _isChasing)
         {
                 // CATCHED THE CHICK
+                OnCatCatched?.Invoke();
                 _catStateController.ChangeState(CatState.Attacking);
                 _isChasing = false;
         }
